e15f03a4585c8569f5ddd3504d609ddecf911c82
Jeninson Peralta <jeninson@gmail.com>
2025-04-02 21:22:22 -0500

    Inicios de JavaScript y Login con IAG

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-alert("ok")
-console.log("ok")
-console.info("Informacion")
+//alert("ok")
+function mostrar(){
+    console.log("ok")
+    let $resp = document.querySelector('#user')
+    console.info($resp.value)
+    let $resp2 = document.getElementById("clave")
+    console.log($resp2.value)
+}
