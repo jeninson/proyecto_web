99b6fa5cf1958e3d3802e1cc8fc096d7f6a7d814
Jeninson Peralta <jeninson@gmail.com>
2025-04-23 20:05:17 -0500

    Trabajando JS con Module

---
+++ js/login.js
@@ -0,0 +1,26 @@
+
+export function validarLogin(){
+    //console.log("ok")
+    //Expresion regular para validar el correo electronico
+    let regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    let $lb_msg = document.querySelector("#lb_msg")
+    let user = document.querySelector("#user").value
+    let clave = document.querySelector("#clave").value
+    let msg = "";
+    $lb_msg.innerHTML = "<div class='spinner-border text-primary' role='status'><span class='sr-only'>Proceso de validacion...</span></div>"
+    if(user.trim() === "" || !regEx.test(user)) msg = "El usuario ingresado es invalido.";
+    if(clave.trim() === "") msg = "La contraseña ingresada es invalida."
+    
+    if(msg !== ""){
+        $lb_msg.innerHTML = `<strong class='text-danger'>${msg}</strong>`
+    }
+
+    setTimeout(() => {
+        $lb_msg.innerHTML = "&nbsp;"
+    }, 3000)
+
+    console.log(user, clave)
+}
+
+export function mostrar(){}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+import { validarLogin } from "./login.js"
+
 document.addEventListener("DOMContentLoaded", (e)=>{
     inicio()
 })
@@ -15,27 +17,3 @@ document.addEventListener("submit", (e)=>{
 })
 
 function inicio(){ console.log("Inicio de la Pagina")}
-
-function validarLogin(){
-    console.log("ok")
-    //Expresion regular para validar el correo electronico
-    let regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-
-    let $lb_msg = document.querySelector("#lb_msg")
-    let user = document.querySelector("#user").value
-    let clave = document.querySelector("#clave").value
-    let msg = "";
-    $lb_msg.innerHTML = "Proceso de validacion..."
-    if(user.trim() === "" || !regEx.test(user)) msg = "El usuario ingresado es invalido."
-    if(clave.trim() === "") msg = "La contraseña ingresada es invalida."
-    
-    if(msg !== ""){
-        $lb_msg.innerHTML = "<strong class='text-danger'>"+msg+"</strong>"
-        setTimeout(() => {
-            $lb_msg.innerHTML = ""
-        }, 3000)
-        return
-    }
-
-    console.log(user, clave)
-}
